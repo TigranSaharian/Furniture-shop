f7abc852452cc5aed19d532509c14f24bd8da47f
Tigran Saharyan <tigran.saharyan@gmail.com>
2021-03-21 01:19:41 +0400

    added about page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 (function(){
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+
     $(window).scroll(function() {
         if ($(document).scrollTop() > 70) {
             $('#menu-navbar').addClass('menu-navbar-position');
@@ -7,4 +10,9 @@
             $('#menu-navbar').removeClass('menu-navbar-position');
         }
     });
+
+    output.innerHTML = slider.value;
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+    }
 })()
